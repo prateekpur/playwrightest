c04baea492431d3a46e7072b89accbacfb4dd99b
Prateek Puri <prateekpuri@Prateeks-MacBook-Pro.local>
2024-11-15 20:55:02 +0530

    fix

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -1,10 +1,36 @@
 import { Page, expect, test } from "@playwright/test";
+import { login } from "./helpers";
 
 test("Tet sign-in", async ({ browser }) => {
+  //const run = require('./scale-test');
+
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  page.setDefaultTimeout(120000);
+  // login(page);
+
+  page.setDefaultTimeout(180000);
+  //log in
+  await page.goto("https://app.proposify.net/login");
+  await page.locator("#pyLoginEmail").fill("fe.testing+1700946230641@proposify.com");
+  await page.locator("#pyLoginPassword").fill("VBJ!qhj@tzk3edg4zum");
+  await page.locator("#pyLoginFormSubmitButton").click();
+
+  //create document
+  await page.locator('button[data-testid="create-document-button"]').click();
+  await page.locator("*[data-testid='create-document-button-v3']").click();
+  await page.getByTestId("start-from-scratch-button").click();
+
+  //wait for canvas to be displayed
+  const canvasLocator = page.locator(".editor__page");
+  await page.waitForLoadState("networkidle");
+  canvasLocator.waitFor({ state: "visible" });
+
+  //click logoout
+  await page.locator(".ant-dropdown-trigger").click();
+  await page.locator("[href*='logout']").click();
+
+  /*page.setDefaultTimeout(120000);
 
   // Go to the login page
   //console.log('Begin');
@@ -25,6 +51,12 @@ test("Tet sign-in", async ({ browser }) => {
 
   // Close the browser
   await browser.close();
-  //console.log('Closed');
+  //console.log('Closed');*/
 });
 
+/*test("Table row re-order", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+
+});*/
